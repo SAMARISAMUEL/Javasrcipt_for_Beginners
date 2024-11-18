236b072fe62e4af02bfd1a77d37db8b3dd83b03e
samari samuel <liveogd@yahoo.com>
2024-11-18 09:37:44 +0100

    feat: javascript data types

---
+++ new.js
@@ -0,0 +1,16 @@
+// const mybody = document.body;
+// const myDiv = document.createElement("div");
+// const mypara = document.createElement("h1");
+// const myimg = document.createElement("img");
+// myimg.src = "./chome.jpg";
+// myimg.classList.add("myImg");
+// mypara.textContent = "This is a new div";
+
+// myDiv.classList.add("mynewDiv");
+
+// myDiv.append(mypara);
+
+// myDiv.append(myimg);
+
+// mybody.append(myDiv);
+console.log(firstName + " " + lastName);
