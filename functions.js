e1c355c6e7bdfd00f85f3cf413fd3144b83590e2
samari samuel <liveogd@yahoo.com>
2024-11-23 10:22:39 +0100

    feat: functions

---
+++ functions.js
@@ -0,0 +1,15 @@
+// function sum(a, b) {
+//   result = a + b;
+//   console.log(result);
+// }
+// sum(100, 60);
+// const addnumber = (a, b) => {
+//   console.log(a + b); // This will log the result to the console.
+// };
+// addnumber(120, 100);
+function fullName() {
+  let a = 10;
+  let b = 25;
+
+  console.log(a + b);
+}
